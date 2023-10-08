07fd886a78f4c8e5004acec5e117de288fc00936
Zach Ramsey <zramsey11@gmail.com>
2023-10-08 13:44:37 -0500

    Selector & Countdown Operational
    Timer counts down from selected initial value as expected; displays each value & ends at "--".

--- main.asm
+++ main.asm
@@ -1,5 +1,5 @@
 ;====================================
-; StopwatchLab2.asm
+; RPG_Lab03.asm
 ;
 ; Created: 9/12/2023 7:02:54 PM
 ; Authors: Trey Vokoun & Zach Ramsey
@@ -23,16 +23,15 @@ cbi DDRD,6	; Board Pin 6 RPG A -> Board I/P: PD6
 cbi DDRD,5  ; Board Pin 5 RPG B -> Board I/P: PD5
 
 
-;========| Configure custom state register |=========
-.def Disp_Queue_0 = R16		; Data queue for next digit to be displayed
-.def Disp_Queue_1 = R17		; Data queue for next digit to be displayed
-.def Disp_Decr = R18		; Count of remaining bits to be pushed from Disp_Queue; decrements from 8
-.def Digit_Count = R19		; Keeps track of which values are to be displayed
-.def Ctrl_Reg = R20			; Custom state register
-.def RPG_Curr = R21			; Current RPG input state
-.def RPG_Bckp = R22			; Backup current RPG input state
-.def RPG_Prev = R23			; previous RPG input state
-.def Ptrn_Cnt = R24			; Pattern counter
+;==============| Configure Registers |===============
+.def Disp_Queue = R16		; Data queue for next digit to be displayed
+.def Disp_Decr = R17		; Count of remaining bits to be pushed from Disp_Queue; decrements from 8
+.def RPG_Curr = R18			; Current RPG input state
+.def RPG_Prev = R19			; previous RPG input state
+.def Ptrn_Cnt = R20			; Pattern counter
+.def Tmp_Reg = R21			; Temporary register
+.def Tmr_Cnt = R22		; Timer counter
+.def Ctrl_Reg = R23			; Custom state register
 
 ; Custom state register masks
 .equ PB_State = 0x01		; bit 0: button A was pressed   (0:None     | 1:Pressed)
@@ -57,143 +56,118 @@ cbi DDRD,5  ; Board Pin 5 RPG B -> Board I/P: PD5
 ;---------------
 
 
-;=========| Load Values to Digit_Patterns |==========
+;=========| Load Digit Patterns |==========
 rjmp Init	; don't execute data!
 Ptrns:
 	.dw 0x4040	; --
-	.dw 0x3F3F	; 00
-	.dw 0x3F06	; 01
-	.dw 0x3F5B	; 02
-	.dw 0x3F4F	; 03
-	.dw 0x3F66	; 04
-	.dw 0x3F6D	; 05
-	.dw 0x3F7D	; 06
-	.dw 0x3F07	; 07
-	.dw 0x3F7F	; 08
-	.dw 0x3F67	; 09
-	.dw 0x063F	; 10
-	.dw 0x0606	; 11
-	.dw 0x065B	; 12
-	.dw 0x064F	; 13
-	.dw 0x0666	; 14
-	.dw 0x066D	; 15
-	.dw 0x067D	; 16
-	.dw 0x0607	; 17
-	.dw 0x067F	; 18
-	.dw 0x0667	; 19
-	.dw 0x5B3F	; 20
-	.dw 0x5B06	; 21
-	.dw 0x5B5B	; 22
-	.dw 0x5B4F	; 23
-	.dw 0x5B66	; 24
-	.dw 0x5B6D	; 25
-	.dw 0x5B7D	; 26
-	.dw 0x5B07	; 27
-	.dw 0x5B7F	; 28
-	.dw 0x5B67	; 29
-	.dw 0x4F3F	; 30
-	.dw 0x4F06	; 31
-	.dw 0x4F5B	; 32
-	.dw 0x4F4F	; 33
-	.dw 0x4F66	; 34
-	.dw 0x4F6D	; 35
-	.dw 0x4F7D	; 36
-	.dw 0x4F07	; 37
-	.dw 0x4F7F	; 38
-	.dw 0x4F67	; 39
-	.dw 0x663F	; 40
-	.dw 0x6606	; 41
-	.dw 0x665B	; 42
-	.dw 0x664F	; 43
-	.dw 0x6666	; 44
-	.dw 0x666D	; 45
-	.dw 0x667D	; 46
-	.dw 0x6607	; 47
-	.dw 0x667F	; 48
-	.dw 0x6667	; 49
-	.dw 0x6D3F	; 50
-	.dw 0x6D06	; 51
-	.dw 0x6D5B	; 52
-	.dw 0x6D4F	; 53
-	.dw 0x6D66	; 54
-	.dw 0x6D6D	; 55
-	.dw 0x6D7D	; 56
-	.dw 0x6D07	; 57
-	.dw 0x6D7F	; 58
-	.dw 0x6D67	; 59
+	.dw 0x3F3F, 0x3F06, 0x3F5B, 0x3F4F, 0x3F66, 0x3F6D, 0x3F7D, 0x3F07, 0x3F7F, 0x3F67	; 0s
+	.dw 0x063F, 0x0606, 0x065B, 0x064F, 0x0666, 0x066D, 0x067D, 0x0607, 0x067F, 0x0667	; 10s
+	.dw 0x5B3F, 0x5B06, 0x5B5B, 0x5B4F, 0x5B66, 0x5B6D, 0x5B7D, 0x5B07, 0x5B7F, 0x5B67	; 20s
+	.dw 0x4F3F, 0x4F06, 0x4F5B, 0x4F4F, 0x4F66, 0x4F6D, 0x4F7D, 0x4F07, 0x4F7F, 0x4F67	; 30s
+	.dw 0x663F, 0x6606, 0x665B, 0x664F, 0x6666, 0x666D, 0x667D, 0x6607, 0x667F, 0x6667	; 40s
+	.dw 0x6D3F, 0x6D06, 0x6D5B, 0x6D4F, 0x6D66, 0x6D6D, 0x6D7D, 0x6D07, 0x6D7F, 0x6D67	; 50s
 	.dw 0x7D3F	; 60
 
 
 ;===================| Main Loop |====================
 Init:
-	ldi Ctrl_Reg, 0x00		; initialize Ctrl_Reg
-	ldi Ptrn_Cnt, 0x00		; initialize pattern counter
+	; initialize timer0; we want 1s intervals
+	; |	Time = 2^bits / (clock/prescaler) * counter
+	; |	Time = 2^8 / (16MHz/1024) * 61 = 0.999424s
+	; |	%E = [(1s - 0.999424s) / 1s] * 100 = 0.0576%
+	ldi Tmr_Cnt, 61		; load timer counter
+	ldi Tmp_Reg, 0x05		; configure prescaler to 1024
+	out TCCR0B, Tmp_Reg	; output configuration to TCCR0B
+
+	; initialize Z pointer to Ptrns
 	ldi R30, low(Ptrns<<1)	; Load low byte of Patterns address
 	ldi R31, high(Ptrns<<1)	; Load high byte of Patterns address
-	lpm Disp_Queue_0, Z+	; load first pattern
-	rcall display
-	lpm Disp_Queue_0, Z		; load second pattern
-	rcall display
+	rcall Load_Pattern		; load initial pattern
+
+	; clear registers
+	clr Ctrl_Reg			; initialize Ctrl_Reg
+	clr Ptrn_Cnt			; initialize pattern counter
 
 Main:
+	; check for button press
 	sbis PIND,7				; If PB is pressed -> Jump to Pressed
 	rjmp Pressed
 
+	; check for rotation
 	in RPG_Curr, PIND
 	andi RPG_Curr, 0x60		; Mask bits 6 and 5
 	cpi RPG_Curr, 0x60		; if both are set, jump to RPG_Detent
 	breq RPG_Detent
 	mov RPG_Prev, RPG_Curr	; otherwise update previous input state
 
-	rjmp Main				; loop back to main
+	rjmp Main				; loop Main
+	
 
 ;===================| Functions |====================
-Pressed:
-	sbr Ctrl_Reg, PB_State
-	sbis PIND, 7			; if PB released, skip
-	rjmp Pressed
-	ldi Disp_Queue_0, 0x7C	; <- replace w/ pushbutton functionality from here
-	rcall display
-	ldi Disp_Queue_0, 0x73
-	rcall display			; <- to here
-	rjmp Main
-	
-RPG_Detent:
-	cpi RPG_Prev, 0x20 		; if prev state was '01', jump to CW
-	breq CW
-	cpi RPG_Prev, 0x40 		; if prev state was '10', jump to CCW
-	breq CCW
-	rjmp Main				; otherwise, jump to Main
-CW:
-	cpi Ptrn_Cnt, 61		; if pattern counter is at end of array, jump to main
-	breq Main
-	inc Ptrn_Cnt			; increment pattern counter
-	adiw zh:zl, 1			; increment Z pointer
-	rjmp Load_Pattern
-CCW:
+Running:
+	in Tmp_Reg, TIFR0		; load timer0 interrupt flag register
+	sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
+	rjmp Running
+
+	ldi Tmp_Reg, (1<<TOV0)	; clear overflow flag by setting logic 1?
+	out TIFR0, Tmp_Reg		; store timer0 interrupt flag register
+	dec Tmr_Cnt				; decrement timer counter
+	brne Running			; if timer counter is not 0, jump to Running
+	ldi Tmr_Cnt, 61			; otherwise, reload timer counter
+
 	cpi Ptrn_Cnt, 0			; if pattern counter is at beginning of array, jump to main
 	breq Main
 	dec Ptrn_Cnt			; decrement pattern counter
 	sbiw zh:zl, 3			; decrement Z pointer
-Load_Pattern:
-	lpm Disp_Queue_0, Z+	; load first byte of word
-	rcall display
-	lpm Disp_Queue_0, Z		; load second byte of word
-	rcall display
+	rcall Load_Pattern
+	rjmp Running
+
+Pressed:
+	sbis PIND, 7			; if PB still pressed, jump to Pressed
+	rjmp Pressed			
+	rjmp Running			; otherwise, jump to Running
+	
+RPG_Detent:
+	cpi RPG_Prev, 0x20 		; if prev state was '01', jump to Incr
+	breq Incr
+	cpi RPG_Prev, 0x40 		; if prev state was '10', jump to Decr
+	breq Decr
+	rjmp Main				; otherwise, jump to Main
+Incr:
 	ldi RPG_Prev, 0x60		; set detent input state
+	cpi Ptrn_Cnt, 61		; if pattern counter is at end of array, jump to main
+	breq Main
+	inc Ptrn_Cnt			; increment pattern counter
+	adiw zh:zl, 1			; increment Z pointer
+	rcall Load_Pattern
+	rjmp Main
+Decr:
+	ldi RPG_Prev, 0x60		; set detent input state
+	cpi Ptrn_Cnt, 0			; if pattern counter is at beginning of array, jump to main
+	breq Main
+	dec Ptrn_Cnt			; decrement pattern counter
+	sbiw zh:zl, 3			; decrement Z pointer
+	rcall Load_Pattern
 	rjmp Main
 
 
-;============| Display Digit Subroutine |============
+;===============| Display Subroutines |===============
+Load_Pattern:
+	lpm Disp_Queue, Z+		; load first byte of word
+	rcall display
+	lpm Disp_Queue, Z		; load second byte of word
+	rcall display
+	ret
+
 display:
 	; backup used registers on stack
-	push Disp_Queue_0		; Push Disp_Queue to stack
+	push Disp_Queue		; Push Disp_Queue to stack
 	push Disp_Decr			; Push Disp_Decr to stack
 	in Disp_Decr, SREG		; Input from SREG -> Disp_Decr
 	push Disp_Decr			; Push Disp_Decr to stack
 	ldi Disp_Decr, 8		; loop -> test all 8 bits
 loop:
-	rol Disp_Queue_0		; rotate left through Carry
+	rol Disp_Queue		; rotate left through Carry
 	BRCS set_ser			; branch if Carry is set
 	cbi PORTB,0				; clear SER (SER -> 0)
 	rjmp end
@@ -216,5 +190,5 @@ end:
 	pop Disp_Decr
 	out SREG, Disp_Decr
 	pop Disp_Decr
-	pop Disp_Queue_0
+	pop Disp_Queue
 	ret
