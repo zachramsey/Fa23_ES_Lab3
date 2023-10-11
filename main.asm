619e0890e87bd778d4e50d08276e22adf80f5bbd
Zach Ramsey <zramsey11@gmail.com>
2023-10-10 21:19:55 -0500

    all done yay

--- main.asm
+++ main.asm
@@ -5,23 +5,20 @@
 ; Authors: Trey Vokoun & Zach Ramsey
 ;====================================
 
-.include "m328Pdef.inc" ; microcontroller-specific definitions
+.include "m328Pdef.inc"		; microcontroller-specific definitions
 .cseg
 .org 0
 
-
 ;==================| Configure I/O |=================
 ; Output to shiftreg SN74HC595
-sbi DDRB,0	; Board Pin 8 O/P: PB0 -> ShiftReg I/P: SER
-sbi DDRB,1	; Board Pin 9 O/P: PB1 -> ShiftReg I/P: RCLK
-sbi DDRB,2	; Board Pin 10 O/P: PB2 -> ShiftReg I/P: SRCLK
-sbi DDRB,3  ; Board Pin 11 O/P: PB3 -> Status LEDs
-
+sbi DDRB,0					; Board Pin 8 O/P: PB0 -> ShiftReg I/P: SER
+sbi DDRB,1					; Board Pin 9 O/P: PB1 -> ShiftReg I/P: RCLK
+sbi DDRB,2					; Board Pin 10 O/P: PB2 -> ShiftReg I/P: SRCLK
+sbi DDRB,3  				; Board Pin 11 O/P: PB3 -> Status LEDs
 ; Input from pushbuttons
-cbi DDRD,7	; Board Pin 7 Pushbutton A -> Board I/P: PD7
-cbi DDRD,6	; Board Pin 6 RPG A -> Board I/P: PD6
-cbi DDRD,5  ; Board Pin 5 RPG B -> Board I/P: PD5
-
+cbi DDRD,7					; Board Pin 7 Pushbutton A -> Board I/P: PD7
+cbi DDRD,6					; Board Pin 6 RPG A -> Board I/P: PD6
+cbi DDRD,5  				; Board Pin 5 RPG B -> Board I/P: PD5
 
 ;==============| Configure Registers |===============
 .def Disp_Queue = R16		; Data queue for next digit to be displayed
@@ -30,35 +27,11 @@ cbi DDRD,5  ; Board Pin 5 RPG B -> Board I/P: PD5
 .def RPG_Prev = R19			; previous RPG input state
 .def Ptrn_Cnt = R20			; Pattern counter
 .def Tmp_Reg = R21			; Temporary register
-.def Tmr_Cnt = R22		; Timer counter
-.def Btn_Cnt = R23		; Button timer counter
-.def Ctrl_Reg = R24			; Custom state register
-
-; Custom state register masks
-.equ PB_State = 0x01		; bit 0: button A was pressed   (0:None     | 1:Pressed)
-.equ Prev_RPG_A = 0x02		; bit 1: A_RPG activation		(0:Inactive | 1:Active)
-.equ Prev_RPG_B = 0x04		; bit 2: B_RPG activation		(0:Inactive | 1:Active)
-.equ Run_State = 0x08		; bit 3: incrementing state     (0:Stopped  | 1:Running)
-.equ Reset_State = 0x10		; bit 4: reset state            (0:None     | 1:Reset)
-.equ Ovrflw = 0x20			; bit 5: overflow state         (0:None     | 1:Overflow)
-
-;-----Usage-----
-; Set State:
-; sbr Ctrl_Reg, (state mask)
-
-; Clear State:
-; cbr Ctrl_Reg, (state mask)
-
-; Skip next if bit is 0:
-; sbrc Ctrl_Reg, (reg bit #)
-
-; Skip next if bit is 1:
-; sbrs Ctrl_Reg, (reg bit #)
-;---------------
-
+.def Tmr_Cnt = R22			; Timer counter
+.def Btn_Cnt = R23			; Button timer counter
 
 ;=========| Load Digit Patterns |==========
-rjmp Init	; don't execute data!
+rjmp Init					; don't execute data!
 Ptrns:
 	.dw 0x4040	; --
 	.dw 0x3F3F, 0x3F06, 0x3F5B, 0x3F4F, 0x3F66, 0x3F6D, 0x3F7D, 0x3F07, 0x3F7F, 0x3F67	; 0s
@@ -69,73 +42,70 @@ Ptrns:
 	.dw 0x6D3F, 0x6D06, 0x6D5B, 0x6D4F, 0x6D66, 0x6D6D, 0x6D7D, 0x6D07, 0x6D7F, 0x6D67	; 50s
 	.dw 0x7D3F	; 60
 
-
 ;===================| Main Loop |====================
 Init:
-	; initialize timer0; we want 1s intervals
-	; |	Time = 2^bits / (clock/prescaler) * counter
-	; |	Time = 2^8 / (16MHz/1024) * 61 = 0.999424s
-	; |	%E = [(1s - 0.999424s) / 1s] * 100 = 0.0576%
-	ldi Tmr_Cnt, 61		; load timer counter
+	cbi PORTB, 3			; clear indicator LEDs
+	; init timer0
 	ldi Tmp_Reg, 0x05		; configure prescaler to 1024
-	out TCCR0B, Tmp_Reg	; output configuration to TCCR0B
-
-	ldi Btn_Cnt, 61		; load button timer counter
-
-	; initialize Z pointer to Ptrns
+	out TCCR0B, Tmp_Reg		; output configuration to TCCR0B
+	; init Z pointer to Ptrns
 	ldi R30, low(Ptrns<<1)	; Load low byte of Patterns address
 	ldi R31, high(Ptrns<<1)	; Load high byte of Patterns address
 	rcall Load_Pattern		; load initial pattern
-
-	; clear registers
-	clr Ctrl_Reg			; initialize Ctrl_Reg
-	clr Ptrn_Cnt			; initialize pattern counter
+	; init various counters
+	ldi Ptrn_Cnt, 0			; init pattern counter to 0
+	ldi Tmr_Cnt, 61			; init timer counter to 61
+	ldi Btn_Cnt, 61			; init button counter to 61
 
 Main:
 	; check for button press
 	sbis PIND,7				; If PB is pressed -> Jump to Pressed
 	rjmp Pressed
-
-	; check for rotation
+	; check for RPG rotation
 	in RPG_Curr, PIND
 	andi RPG_Curr, 0x60		; Mask bits 6 and 5
 	cpi RPG_Curr, 0x60		; if both are set, jump to RPG_Detent
 	breq RPG_Detent
 	mov RPG_Prev, RPG_Curr	; otherwise update previous input state
+	; loop Main
+	rjmp Main
 
-	rjmp Main				; loop Main
-	
-
-;===================| Functions |====================
+;===============| Running Functions |================
 Running:
-	in Tmp_Reg, TIFR0		; load timer0 interrupt flag register
+	in Tmp_Reg, TIFR0		; input timer0 interrupt flag register
 	sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
 	rjmp Running
-
-	sbis PIND, 7			; if PB is pressed, decrement button timer counter
-	dec Btn_Cnt
-	tst Btn_Cnt				; if button timer counter is 0, jump to Reset
-	breq Init				; if button timer counter is 0, jump to Init
-	ldi Btn_Cnt, 61			; otherwise, reload button timer counter
-
-	ldi Tmp_Reg, (1<<TOV0)	; clear overflow flag by setting logic 1?
-	out TIFR0, Tmp_Reg		; store timer0 interrupt flag register
+	ldi Tmp_Reg, (1<<TOV0)	; acknowledge overflow flag
+	out TIFR0, Tmp_Reg		; output to timer0 interrupt flag register
+	tst Btn_Cnt				; if button timer counter is not 0, jump to Wait
+	brne Wait
+	sbic PIND, 7			; else if PB is not pressed, reset clock
+	rjmp Init
+Countdown:
 	dec Tmr_Cnt				; decrement timer counter
 	brne Running			; if timer counter is not 0, jump to Running
 	ldi Tmr_Cnt, 61			; otherwise, reload timer counter
-
-	cpi Ptrn_Cnt, 0			; if pattern counter is at beginning of array, jump to main
-	breq Main
+	cpi Ptrn_Cnt, 1			; if pattern counter is at 0, reset clock
+	breq Init
 	dec Ptrn_Cnt			; decrement pattern counter
 	sbiw zh:zl, 3			; decrement Z pointer
 	rcall Load_Pattern
 	rjmp Running
+Wait:
+	sbis PIND, 7			; if PB is pressed, decrement btn_cnt
+	dec Btn_Cnt
+	sbic PIND, 7			; if PB is not pressed, reset btn_cnt
+	ldi Btn_Cnt, 61
+	rjmp Countdown
 
+;===============| Stopped Functions |================
 Pressed:
+	cpi Ptrn_Cnt, 0			; if pattern counter is at 0, jump to Main
+	breq Main
 	sbis PIND, 7			; if PB still pressed, jump to Pressed
-	rjmp Pressed			
+	rjmp Pressed
+	sbi PORTB, 3
 	rjmp Running			; otherwise, jump to Running
-	
 RPG_Detent:
 	cpi RPG_Prev, 0x20 		; if prev state was '01', jump to Incr
 	breq Incr
@@ -159,7 +129,6 @@ Decr:
 	rcall Load_Pattern
 	rjmp Main
 
-
 ;===============| Display Subroutines |===============
 Load_Pattern:
 	lpm Disp_Queue, Z+		; load first byte of word
@@ -167,16 +136,15 @@ Load_Pattern:
 	lpm Disp_Queue, Z		; load second byte of word
 	rcall display
 	ret
-
 display:
 	; backup used registers on stack
-	push Disp_Queue		; Push Disp_Queue to stack
+	push Disp_Queue			; Push Disp_Queue to stack
 	push Disp_Decr			; Push Disp_Decr to stack
 	in Disp_Decr, SREG		; Input from SREG -> Disp_Decr
 	push Disp_Decr			; Push Disp_Decr to stack
 	ldi Disp_Decr, 8		; loop -> test all 8 bits
 loop:
-	rol Disp_Queue		; rotate left through Carry
+	rol Disp_Queue			; rotate left through Carry
 	BRCS set_ser			; branch if Carry is set
 	cbi PORTB,0				; clear SER (SER -> 0)
 	rjmp end
@@ -189,12 +157,10 @@ end:
 	cbi PORTB,2				; SRCLK off
 	dec Disp_Decr
 	brne loop
-	
 	; generate RCLK pulse
 	sbi PORTB,1				; RCLK on
 	nop						; pause to help circuit catch up
 	cbi PORTB,1				; RCLK off
-
 	; restore registers from stack
 	pop Disp_Decr
 	out SREG, Disp_Decr
